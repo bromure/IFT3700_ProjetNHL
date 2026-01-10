c22e467904ec2b45cf2acf918fae3f530358cf42
bromure <xgdrolet@gmail.com>
2026-01-10 17:06:07 -0500

    Fixing Q2 (simple visualisations)
    Fixing the ipywidget interactiveness. Now updated for matches until 2025.

--- Visualisation/question_1.py
+++ Visualisation/question_1.py
@@ -4,7 +4,7 @@ def get_player_stats(year: int, player_type: str) -> pd.DataFrame:
     """
 
     Uses Pandas' built in HTML parser to scrape the tabular player statistics from
-    https://www.hockey-reference.com/leagues/ . If the player played on multiple 
+    https://www.hockey-reference.com/leagues/. If the player played on multiple 
     teams in a single season, the individual team's statistics are discarded and
     the total ('TOT') statistics are retained (the multiple team names are discarded)
 
